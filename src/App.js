f3b711d161aeaf1c11b9dd1aaefef7cfb3dad9f4
Jody Lorist <66092262+jody29@users.noreply.github.com>
2021-12-15 18:16:45 +0100

    map rendered

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import './App.css';
-import ReactMapGL from 'react-map-gl'
-import { useState } from 'react';
+import Map from './map';
+
 
 function App() {
-  const [viewport, setViewport] = useState({
-    latitude: 52.50249,
-    longitude: 4.96199,
-    width: '100vw',
-    height: '40em',
-    zoom: 12.5,
-    pitch: 30,
-    bearing: 0
-  })
-
   return (
     <>
-    <h1>Gevonden afval in Nederland (test)</h1>
-      <ReactMapGL
-        {...viewport}
-        mapStyle="mapbox://styles/mapbox/dark-v10"
-        mapboxApiAccessToken='pk.eyJ1Ijoiam9keTU2OSIsImEiOiJja3g3amJ5MGowMW8wMm5zZTlwN3Fjb2t0In0.99DjUaNvteP2DPXThnnHXg'
-        onViewportChange={viewport => {
-          setViewport(viewport)
-        }}
-      ></ReactMapGL>
+      <Map />
     </>
   );
 }
