9effb97ef7e4eaf26477d4ad5b90568cdbf6af86
Jody Lorist <66092262+jody29@users.noreply.github.com>
2021-12-15 16:33:47 +0100

    map added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import ReactMapGL from 'react-map-gl'
+import { useState } from 'react';
 
 function App() {
+  const [viewport, setViewport] = useState({
+    latitude: 52.50249,
+    longitude: 4.96199,
+    width: '100vw',
+    height: '40em',
+    zoom: 12.5,
+    pitch: 30,
+    bearing: 0
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Gevonden afval in Nederland</h1>
+      <ReactMapGL
+        {...viewport}
+        mapStyle="mapbox://styles/mapbox/dark-v10"
+        mapboxApiAccessToken='pk.eyJ1Ijoiam9keTU2OSIsImEiOiJja3g3amJ5MGowMW8wMm5zZTlwN3Fjb2t0In0.99DjUaNvteP2DPXThnnHXg'
+        onViewportChange={viewport => {
+          setViewport(viewport)
+        }}
+      ></ReactMapGL>
+    </>
   );
 }
 
