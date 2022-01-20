fdcff8ce0ce1ee1a88bb61241597c0ee2ca85e0e
Jody Lorist <66092262+jody29@users.noreply.github.com>
2022-01-20 18:04:22 +0100

    added buttons

--- src/components/more.js
+++ src/components/more.js
@@ -7,10 +7,10 @@ const More = () => {
             <h2>Meer weten over Zwerfinator?</h2>
             <p>Er zijn nog veel meer projecten van Dirk Groot.</p>
             <p>Zwerfinator heeft een antiflu campagne gevoert waardoor antaflu naar een papieren verpakking is veranderd.</p>
-            <Link to='/' className='meerInfo'><p>Meer informatie</p></Link>
+            <Link to='/' className='meerInfo'>Meer informatie</Link>
             <p>Wil je zelf een impact gaan maken in je buurt?</p>
             <p>Lees hier hoe je zelf actie kan gaan ondernemen.</p>
-            <Link to='/' className='meerInfo'><p>Meer informatie</p></Link>
+            <Link to='/' className='meerInfo'>Meer informatie</Link>
         </div>
     )
 }
