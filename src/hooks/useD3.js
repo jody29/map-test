f0a0dbccb040ae70c96b45e07e56740ac3b47a8b
Jody Lorist <66092262+jody29@users.noreply.github.com>
2022-01-20 23:36:24 +0100

    added d3 hook

---
+++ src/hooks/useD3.js
@@ -0,0 +1,14 @@
+import React from "react";
+import * as d3 from "d3";
+
+const useD3 = (renderChartFn) => {
+    const ref = React.useRef();
+  
+    React.useEffect(() => {
+        renderChartFn(d3.select(ref.current));
+        return () => {};
+    });
+    return ref;
+};
+
+export default useD3;
