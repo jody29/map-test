eebbfd2f2f0115304f7d0646c39b7708e5ea10ec
Jody Lorist <66092262+jody29@users.noreply.github.com>
2022-01-20 16:09:40 +0100

    added more info

--- src/components/more.js
+++ src/components/more.js
@@ -1,9 +1,16 @@
+import { Link } from 'react-router-dom'
 import './more.css'
 
 const More = () => {
     return (
         <div className="more">
             <h2>Meer weten over Zwerfinator?</h2>
+            <p>Er zijn nog veel meer projecten van Dirk Groot.</p>
+            <p>Zwerfinator heeft een antiflu campagne gevoert waardoor antaflu naar een papieren verpakking is veranderd.</p>
+            <Link to='/' className='meerInfo'><p>Meer informatie</p></Link>
+            <p>Wil je zelf een impact gaan maken in je buurt?</p>
+            <p>Lees hier hoe je zelf actie kan gaan ondernemen.</p>
+            <Link to='/' className='meerInfo'><p>Meer informatie</p></Link>
         </div>
     )
 }
