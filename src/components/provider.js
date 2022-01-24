c7a817fb6fad184b0c9074704ebeac1780841329
Jody Lorist <66092262+jody29@users.noreply.github.com>
2022-01-24 02:13:23 +0100

    small change

--- src/components/provider.js
+++ src/components/provider.js
@@ -1,5 +1,5 @@
-import { createContext } from "react";
-import { useState } from "react/cjs/react.development";
+import { createContext, useState } from "react";
+
 
 export const YearContext = createContext()
 
