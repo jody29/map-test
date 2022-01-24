4df0412d0263e1274c92645573842fe4ad13899e
Jody Lorist <66092262+jody29@users.noreply.github.com>
2022-01-24 01:55:52 +0100

    no unused var

--- src/pages/intro.js
+++ src/pages/intro.js
@@ -1,6 +1,6 @@
 import './intro.css'
 import fotoDirk from '../fotoDirk.png'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import IntroText from '../components/introText'
 import path from '../path.svg'
 import Amount from './amount'
